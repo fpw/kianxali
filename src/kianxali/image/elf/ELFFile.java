813dbd1f05041e5cee78b29dcc91039c61e4ba15
Folke <folko@solhost.org>
2014-02-22 23:15:35 +0100

    Implemented ELF imports reading

--- src/kianxali/image/elf/ELFFile.java
+++ src/kianxali/image/elf/ELFFile.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import kianxali.cpu.x86.X86CPU.ExecutionMode;
 import kianxali.cpu.x86.X86CPU.Model;
@@ -18,18 +19,21 @@ import kianxali.image.ImageFile;
 import kianxali.image.Section;
 
 public class ELFFile extends ImageFile {
+    private static final Logger LOG = Logger.getLogger("kianxali.image.elf");
     private final ELFHeader header;
+    private final Map<Long, String> imports;
     private final Map<Long, String> stringTable;
-    private final List<Section> sections;
+    private List<Section> loadedSections;
 
     public ELFFile(Path path) throws IOException {
         super(path);
-        this.header = new ELFHeader(imageFile);
-        this.stringTable = new HashMap<>();
-        this.sections = new ArrayList<>(header.getSectionHeaders().size());
+        this.imports = new HashMap<>();
 
-        loadStrings();
+        header = new ELFHeader(imageFile);
+        stringTable = loadStringTable(header.getStringSection());
         loadSections();
+        loadSymbols();
+        loadRelocations();
     }
 
     public static boolean isELFFile(Path path) throws IOException {
@@ -41,10 +45,10 @@ public class ELFFile extends ImageFile {
         return magic == ELFHeader.ELF_MAGIC;
     }
 
-    private void loadStrings() {
-        SectionHeader stringHeader = header.getStringSection();
+    private Map<Long, String> loadStringTable(SectionHeader stringHeader) {
+        Map<Long, String> res = new HashMap<>();
         if(stringHeader == null) {
-            return;
+            return res;
         }
 
         long startPos = stringHeader.getOffset();
@@ -53,14 +57,16 @@ public class ELFFile extends ImageFile {
         while(imageFile.getPosition() < endPos) {
             long entryPos = imageFile.getPosition() - startPos;
             String entry = imageFile.readString();
-            stringTable.put(entryPos, entry);
+            res.put(entryPos, entry);
         }
+        return res;
     }
 
     private void loadSections() {
+        loadedSections = new ArrayList<>(header.getSectionHeaders().size());
         for(SectionHeader section : header.getSectionHeaders()) {
             if(section.getAddress() == 0) {
-                // only load sections that are actually loaded
+                // only analyze sections that are actually loaded
                 continue;
             }
 
@@ -71,7 +77,67 @@ public class ELFFile extends ImageFile {
             long offset = section.getOffset();
             boolean executable = section.isExecutable();
 
-            sections.add(new ELFSection(name, offset, start, end, executable));
+            loadedSections.add(new ELFSection(name, offset, start, end, executable));
+        }
+    }
+
+    private List<ELFSymbol> readSymbols(SectionHeader symSection) {
+        List<ELFSymbol> res = new ArrayList<>();
+        Map<Long, String> symStrTab = loadStringTable(header.getSectionHeaders().get((int) symSection.getLink()));
+        imageFile.seek(symSection.getOffset());
+        for(int i = 0; i < symSection.getSize() / symSection.getEntrySize(); i++) {
+            ELFSymbol sym = new ELFSymbol(imageFile, header.has64BitHeader());
+            String name = symStrTab.get(sym.getNameIndex());
+            sym.attachName(name);
+            res.add(sym);
+        }
+        return res;
+    }
+
+    private void loadSymbols() {
+        for(SectionHeader section : header.getSectionHeaders()) {
+            if(section.getType() != SectionHeader.Type.SHT_SYMTAB) {
+                // we only want the symbol table here
+                continue;
+            }
+            for(ELFSymbol sym : readSymbols(section)) {
+                if(sym.getValue() != 0 && sym.getAttachedName() != null && sym.getType() == ELFSymbol.Type.STT_FUNC) {
+                    imports.put(sym.getValue(), sym.getAttachedName());
+                }
+            }
+        }
+    }
+
+    private void loadRelocations() {
+        for(SectionHeader section : header.getSectionHeaders()) {
+            if(section.getType() != SectionHeader.Type.SHT_REL && section.getType() != SectionHeader.Type.SHT_RELA) {
+                // only analyze relocation tables
+                continue;
+            }
+            SectionHeader linkedSection = header.getSectionHeaders().get(((int) section.getLink()));
+            if(linkedSection.getType() != SectionHeader.Type.SHT_DYNSYM) {
+                // not sure if that can happen, hopefully not
+                LOG.warning("ELF SHT_REL/A with link not being SHT_DYNSYM");
+                continue;
+            }
+
+            // parse linked DynSym table
+            List<ELFSymbol> symbols = readSymbols(linkedSection);
+
+            imageFile.seek(section.getOffset());
+            for(int i = 0; i < section.getSize() / section.getEntrySize(); i++) {
+                boolean addend = false;
+                if(section.getType() == SectionHeader.Type.SHT_RELA) {
+                    addend = true;
+                }
+                ELFRelocation rel = new ELFRelocation(imageFile, addend, header.has64BitHeader());
+                if(rel.getType() != ELFRelocation.Type.JUMP_SLOT) {
+                    // only analyze jump slots for now
+                    continue;
+                }
+                String name = symbols.get((int) rel.getInfoIndex()).getAttachedName();
+                imports.put(rel.getAddress(), name);
+            }
         }
     }
 
@@ -82,7 +148,7 @@ public class ELFFile extends ImageFile {
 
     @Override
     public List<Section> getSections() {
-        return Collections.unmodifiableList(sections);
+        return Collections.unmodifiableList(loadedSections);
     }
 
     @Override
@@ -103,7 +169,7 @@ public class ELFFile extends ImageFile {
 
     @Override
     public long toMemAddress(long fileOffset) {
-        for(Section section_ : sections) {
+        for(Section section_ : loadedSections) {
             ELFSection section = (ELFSection) section_;
             long size = section.getEndAddress() - section.getStartAddress();
             if(fileOffset >= section.getOffset() && fileOffset <= section.getOffset() + size) {
@@ -116,6 +182,6 @@ public class ELFFile extends ImageFile {
 
     @Override
     public Map<Long, String> getImports() {
-        return new HashMap<Long, String>();
+        return Collections.unmodifiableMap(imports);
     }
 }
