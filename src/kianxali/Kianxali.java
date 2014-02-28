4a7b4c988f0fdd23566c02fb88c4e09eb1f25f7f
Folke Will <fwi@informatik.uni-kiel.de>
2014-02-28 15:54:09 +0100

    - Added JavaDoc

--- src/kianxali/Kianxali.java
+++ src/kianxali/Kianxali.java
@@ -8,10 +8,20 @@ import java.util.logging.Logger;
 import kianxali.gui.Controller;
 import kianxali.util.LogFormatter;
 
+/**
+ * Kianxali is an interactive, scriptable disassembler supporting multiple architectures
+ * and file types.
+ * @author fwi
+ *
+ */
 public final class Kianxali {
     private static final Logger LOG = Logger.getLogger("kianxali");
     private final Controller controller;
 
+    /**
+     * Create a new instance of the disassembler
+     * @param logLevel the log level to display on the console
+     */
     public Kianxali(Level logLevel) {
         // setup logging
         Handler consoleHandler = new ConsoleHandler();
@@ -26,10 +36,17 @@ public final class Kianxali {
         controller = new Controller();
     }
 
+    /**
+     * Starts the actual disassembler by showing the GUI
+     */
     public void start() {
         controller.showGUI();
     }
 
+    /**
+     * Main entry point of the program: Starts the disassembler and shows the GUI
+     * @param args unused
+     */
     public static void main(String[] args) {
         Kianxali kianxali = new Kianxali(Level.FINEST);
         kianxali.start();
